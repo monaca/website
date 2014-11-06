3aa5caa8be2ac673f8b5928237e02ede682700e6
Hitomi Katsuhiro <hitomi@asial.co.jp>
2014-11-06 11:36:53 +0900

    Use Assemble

---
+++ Gruntfile.js
@@ -0,0 +1,149 @@
+/*
+ * Generated on 2014-11-05
+ * generator-assemble v0.5.0
+ * https://github.com/assemble/generator-assemble
+ *
+ * Copyright (c) 2014 Hariadi Hinta
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// '<%= config.src %>/templates/pages/{,*/}*.hbs'
+// use this if you want to match all subfolders:
+// '<%= config.src %>/templates/pages/**/*.hbs'
+
+module.exports = function(grunt) {
+
+    require('time-grunt')(grunt);
+    require('load-grunt-tasks')(grunt);
+
+    // Project configuration.
+    grunt.initConfig({
+
+        config: {
+            src: 'src',
+            dist: 'dist'
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    sourcemap: true
+                },
+                files: [{
+                    expand: true,
+                    cwd   : 'src/templates/sass',
+                    src   : '*.scss',
+                    dest  : 'dist/css/',
+                    ext   : '.css'
+                }]
+            }
+        },
+        watch: {
+            assemble: {
+                files: ['<%= config.src %>/{content,data,templates}/{,*/}*.{md,hbs,yml}'],
+                tasks: ['assemble']
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    '<%= config.dist %>/{,*/}*.html',
+                    '<%= config.dist %>/assets/{,*/}*.css',
+                    '<%= config.dist %>/assets/{,*/}*.js',
+                    '<%= config.dist %>/assets/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            },
+            sass: {
+                files: ['<%= config.src %>/**/*.scss'],
+                tasks: ['sass'],
+                options : {
+                    spawn: false
+                }
+            }
+        },
+
+        connect: {
+            options: {
+                port: 9000,
+                livereload: 35729,
+                // change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    base: [
+                        '<%= config.dist %>'
+                    ]
+                }
+            }
+        },
+
+        assemble: {
+            pages: {
+                options: {
+                    flatten: true,
+                    assets: '<%= config.dist %>/assets',
+                    layout: '<%= config.src %>/templates/layouts/default.hbs',
+                    data: '<%= config.src %>/data/*.{json,yml}',
+                    partials: '<%= config.src %>/templates/partials/*.hbs'
+                },
+                files: {
+                    '<%= config.dist %>/': ['<%= config.src %>/templates/pages/*.hbs']
+                }
+            }
+        },
+
+        copy: {
+            bootstrap: {
+                expand: true,
+                cwd: 'bower_components/bootstrap/dist/',
+                src: '**',
+                dest: '<%= config.dist %>/'
+            },
+            jquery: {
+                expand: true,
+                cwd: 'bower_components/jquery/dist/',
+                src: '**',
+                dest: '<%= config.dist %>/js/'
+            },
+            assets: {
+                expand: true,
+                cwd: 'src/assets/',
+                src: '**',
+                dest: '<%= config.dist %>/'
+            }
+        },
+
+        // Before generating any new files,
+        // remove any previously-created files.
+        clean: ['<%= config.dist %>/**/*.{html,xml}']
+
+    });
+
+    grunt.loadNpmTasks('assemble');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.registerTask('server', [
+        'build',
+        'connect:livereload',
+        'watch'
+    ]);
+
+    grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'sass',
+        'assemble'
+    ]);
+
+    grunt.registerTask('default', [
+        'build'
+    ]);
+
+};
