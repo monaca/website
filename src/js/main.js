c6d3e03a90c243342880537c466cdcd914866ca4
Hitomi Katsuhiro <hitomi@asial.co.jp>
2014-11-07 17:24:23 +0900

    index fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
 "use strict";
 
 $(function() {
+    $("[tile=2]").tile(2);
     $("[tile=3]").tile(3);
     $("[tile=4]").tile(4);
     var timer = null;
     window.addEventListener("resize", function() {
         clearTimeout(timer);
         timer = setTimeout(function() {
+            $("[tile=2]").tile(2);
             $("[tile=3]").tile(3);
             $("[tile=4]").tile(4);
         }, 100);
