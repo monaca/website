5bac78bcab6b9b8bc8632d686152563e73c1a6f2
Katsutoshi Makino <makki@asial.co.jp>
2016-03-11 20:40:13 +0900

    modified price page

--- src/js/pages/pricing.js
+++ src/js/pages/pricing.js
@@ -3,15 +3,21 @@
   window.monacaPages = window.monacaPages || [];
 
   monacaPages["/pricing.html"] = function(loginData) {
+    loginData.autoDisplay = false;
+
     if (loginData.status.inJapan) {
       document.getElementById('personal-plan-box').style.display = 'block';
     } else {
       document.getElementById('developer-plan-box').style.display = 'block';
     }
     document.getElementById('pricing-container').style.display = 'block';
+
+    displayBody();
   };
 
   monacaPages["/pricing-detail.html"] = function(loginData) {
+    loginData.autoDisplay = false;
+
     var tableEl = document.getElementById("compare-cont-table");
     var contEl = document.getElementById("compare-cont");
 
@@ -25,5 +31,13 @@
       (contEl.scrollLeft > 0) ? contEl.classList.add("leftshadow") : contEl.classList.remove("leftshadow");
       (contEl.clientWidth + contEl.scrollLeft < tableEl.clientWidth) ? contEl.classList.add("rightshadow") : contEl.classList.remove("rightshadow");
     }
+
+    if (loginData.status.inJapan) {
+      $('.dev-plan').remove();
+    } else {
+      $('.personal-plan').remove();
+    }
+
+    displayBody();
   };
 })();
