9a6faf8dd371d9e221225a48b71983455bd9aa5e
Bryan Ellis <bryan@asial.co.jp>
2016-03-11 18:43:10 +0900

    Updated English and Japanese Showcase to use Showcase API

--- src/js/pages/great-apps.js
+++ src/js/pages/great-apps.js
@@ -1,196 +1,282 @@
-(function() {
+(function () {
+    window.monacaPages = window.monacaPages || [];
+    monacaPages['/great-apps.html'] = function (loginData) {
+        var loadedPage = 0;
+        var isLoading = true;
+        var isFinalPage = false;
+        var $overlay = $('#modal-overlay');
+        var $appDetail = $('#app-detail-container');
 
-  window.monacaPages = window.monacaPages || [];
+        // Populate List
+        populateFeaturedApps();
+        populateAppList();
 
-  monacaPages["/great-apps.html"] = function (loginData) {
-  
-    var d1 = new $.Deferred;
-    var d2 = new $.Deferred;
-
-    showFeaturedApps( function() { d1.resolve(); } , function() { d1.reject(); } );
-    showNormalApps(0 , function() { d2.resolve(); } , function() { d2.reject(); } ); 
-
-    $.when(d1.promise(),d2.promise()).then(
-      function() {
-        $(window).on("scroll", function(e) {
-          var scrollTop = $(window).scrollTop();
-          var moreEl = $("#more").offset();
-          if (!moreEl) return;
-          var threshold = moreEl.top - $(window).height() + 160;
-          if (scrollTop > threshold) {
-            readMoreCases(e);
-          }
+        $(window).scroll(function () {
+            if ($('body').height() <= ($(window).height() + $(window).scrollTop())) {
+                if (!isLoading && !isFinalPage) {
+                    isLoading = true;
+                    populateAppList(loadedPage);
+                }
+            }
         });
-      } ,
-      function() {
-        alert("Network Error");
-      }
-    );
-    
-  };
 
-  function showFeaturedApps(success,fail) {
-    getApps( { lang : window.LANG, isFeature : true },
-      function(data) {
-        if (data.status == "ok") {
-          var list = data.result;
-          list.forEach( function(app) {
-            var item = createFeaturedAppHtml(app);
-            $('#featured').append(item);
-          } );
+        /*
+         * Click Handlers
+         */
+        $('body').on('click', '.app-detail', getAppDetail);
+        $('body').on('click', '.arrow', function (evt) {
+            var sign = $(this).is('.left') ? 1 : -1;
+            var screen = $appDetail.find('.screen');
+            var scroll = screen.scrollLeft();
 
-//          $('body').on("click", ".casepopup", function(e) {
-//            var $target = $($(this).attr("href"));
-//            if (!$target.length) return;
-//
-//            $target.modal();
-//            return false;
-//          });
-          if (success) success(list);
-        } else {
-          if (fail) fail();
+            scroll -= sign * 200;
+            screen.animate({scrollLeft: scroll}, "slow", "swing");
+
+            return false;
+        });
+
+        $appDetail.on('click', 'a.close', function () {
+            $appDetail.fadeOut();
+            $overlay.fadeOut();
+        });
+        $overlay.on('click', function () {
+            $appDetail.fadeOut();
+            $overlay.fadeOut();
+        });
+
+        /*
+         * Fetch App List
+         */
+        function getAppList(data) {
+            var lang = window.LANG;
+            var url = monacaApi.getBaseUrl() + '/' + lang + '/api/showcase/list';
+
+            var $xhr = $.ajax(url, {
+                type: 'GET',
+                dataType: 'json',
+                contentType: 'text/plain',
+                data: data,
+                xhrFields: {
+                    withCredentials: true
+                }
+            });
+
+            return $xhr;
         }
-      }
-    );  
-  }
 
-  function showNormalApps(page,success,fail) {
-    getApps( { lang : window.LANG, isFeature : false , page : page },
-      function(data) {
-        if (data.status == "ok") {
-          var list = data.result;
-          list.forEach( function(app) {
-            var item = createNormalAppHtml(app);
-            $('#caseicon').append(item);
-          } );
-          if (success) success(list);
-        } else {
-          if (fail) fail();
+        function populateFeaturedApps() {
+            var $xhr = getAppList({
+                isFeature: true,
+                page: 0,
+                limit: 3
+            });
+
+            var $location = $('#featured');
+
+            $xhr.success(function (response) {
+                buildAppList(response, $location);
+            });
         }
-      }
-    );  
 
-  }
+        function populateAppList(page) {
+            var limit = 16;
 
-  readMoreCases.page = 1;
-  function readMoreCases(e) {
-    $(".btn-readmorecases").hide();
-    $(".icn-loading-cases").css({"display": "block"});
-    showNormalApps(readMoreCases.page, function(list){
-      readMoreCases.page += 1;
-      $(".btn-readmorecases").show();
-      $(".icn-loading-cases").hide();
-      if (list.length == 0) $("#case .container .more").hide();
-    } , function(){
-      $(".btn-readmorecases").show();
-      $(".icn-loading-cases").hide();
-      alert("Network error!");
-    });
-  }
+            var $xhr = getAppList({
+                isFeature: false,
+                page: page || 0,
+                limit: limit
+            });
 
-  function getApps(options,success,fail) {
-    var lang = options.lang || 'en';
-    var isFeature = options.isFeature || false;
-    var page = options.page || 0;
-    var limit = options.limit || 10;
+            var $location = $('.app-list');
 
-    $.ajax( {
-      type : "GET",
-      url : monacaApi.getBaseUrl() + "/" + lang + "/api/showcase/list",
-      dataType : "JSON",
-      contentType : "text/plain",
-      data : { isFeature : isFeature , page : page , limit : limit } ,
-      xhrFields: {
-        withCredentials: true
-      },
-      success : success,
-      fail : fail
-    } ); 
-  }
+            $xhr.success(function (response) {
+                buildAppList(response, $location);
+                loadedPage++;
+                isLoading = false;
 
-  function getApp(options,success,fail) {
-    var id = options.id;
-    var lang = options.lang || 'en';
-    
-    $.ajax( {
-      type : "GET",
-      url : monacaApi.getBaseUrl() + "/" + lang + "/api/showcase/" + id,
-      dataType : "JSON",
-      contentType : "text/plain",
-      data : {},
-      xhrFields: {
-        withCredentials: true
-      },
-      success : success,
-      fail : fail
-    } );
-  } 
+                if (response.result.length < limit) {
+                    isFinalPage = true;
+                }
+            });
+        }
 
-  function createFeaturedAppHtml(app) {
-    var item = $('<li></li>');
-    if (app.filter == 'borderradius') {
-      item.addClass('borderradius');
-    }
-    var anchor = $('<a class="casepopup"></a>');
-    anchor.attr('href','#case-' + app.id);
-    var figure = $('<figure></figure>');
-    var img = $('<img></img>');
-    img.attr('src',app.icon);
-    figure.append(img);
-    anchor.append(figure);
-    var p1 = $('<p></p>');
-    p1.text( app.title );
-    var p2 = $('<p></p>');
-    p2.text( app.author );
-    anchor.append(p1);
-    anchor.append(p2);
-    item.append(anchor);
-    anchor.click( function() {
-      showModal(app);
-      return false;
-    } );
-    return item;
-  }
+        function buildAppList(response, $location) {
+            var apps = response.result;
 
-  function createNormalAppHtml(app) {
-    var item = $('<li></li>');
-    if (app.filter == 'borderradius') {
-      item.addClass('borderradius');
-    }
-    var anchor = $('<a class="casepopup"></a>');
-    anchor.attr('href','#case-' + app.id);
-    var figure = $('<figure></figure>');
-    var img = $('<img style="width: 150px;"></img>');
-    img.attr('src',app.icon);
-    figure.append(img);
-    anchor.append(figure);
-    var p1 = $('<p></p>');
-    p1.text( app.title );
-    var p2 = $('<p></p>');
-    p2.text( app.author );
-    anchor.append(p1);
-    anchor.append(p2);
-    item.append(anchor); 
-    anchor.click( function() {
-      showModal(app);
-      return false;
-    } );
-    return item;
-  }
+            apps.forEach(function (app) {
+                var $li = $('<li>');
+                var $a = $('<a>');
+                var $figure = $('<figure>');
+                var $img = $('<img>');
+                var $pTitle = $('<p>');
+                var $pAuthor = $('<p>');
 
-  function showModal(app) {
-    getApp( { id : app.id } , function(json) {
-      alert( JSON.stringify( json ) );
-    } , function () { alert("Network Error"); } );
-  }
+                $li.addClass('borderradius');
 
+                $a.attr({
+                    href: '#',
+                    'class': 'casepopup app-detail'
+                });
+                $a.data('id', app.id);
 
+                $img.attr('src', app.icon);
 
+                $pTitle.text(app.title);
+                $pAuthor.text(app.author);
+
+                $figure.append($img);
+                $a.append($figure);
+                $a.append($pTitle);
+                $a.append($pAuthor);
+
+                $li.append($a);
+
+                $location.append($li);
+            });
+        }
+
+        /*
+         * App Detail Methods
+         */
+        /**
+         *
+         * @param evt
+         */
+        function getAppDetail(evt) {
+            if (evt && !evt.target) {
+                return;
+            }
+
+            var $a = $(evt.target);
+
+            if (evt.target.tagName !== 'A') {
+                $a = $(evt.target).closest('a');
+            }
+
+            var appId = $a.data('id');
+            var lang = window.LANG;
+            var url = monacaApi.getBaseUrl() + '/' + lang + '/api/showcase/' + appId;
+
+            var $xhr = $.ajax(url, {
+                type: 'GET',
+                dataType: 'json',
+                contentType: 'text/plain',
+                xhrFields: {
+                    withCredentials: true
+                }
+            });
+
+            $xhr.success(function (response) {
+                var app = response.result;
+
+                var $detail = $('#app-detail-container');
+
+                $detail.find('.app-icon').attr('src', app.icon);
+                $detail.find('.app-title').text(app.title);
+                $detail.find('.app-author').text(app.author);
+                $detail.find('.app-description').text(app.description);
+
+                createScreenshotList($detail, app.images);
+                createAppStoreList($detail, app.store);
+
+                $detail.show();
+                $overlay.show();
+            });
+
+            return $xhr;
+        }
+
+        /**
+         *
+         * @param $detailContainer
+         * @param images
+         */
+        function createScreenshotList($detailContainer, images) {
+            $detailContainer.find('.app-screenshots').empty();
+
+            images.forEach(function (url) {
+                var $li = $('<li>');
+                var $img = $('<img>');
+
+                $img.attr({
+                    'class': 'screenshot',
+                    src: url,
+                    alt: ''
+                });
+
+                $li.append($img);
+                $detailContainer.find('.app-screenshots').append($li);
+            });
+        }
+
+        /**
+         *
+         * @param $detailContainer
+         * @param stores
+         */
+        function createAppStoreList($detailContainer, stores) {
+            $detailContainer.find('.app-stores').empty();
+
+            for (var store in stores) {
+                var url = stores[store];
+
+                if (url === '') {
+                    continue;
+                }
+
+                var $li = $('<li>');
+                var $a = $('<a>');
+                var $img = $('<img>');
+
+                $a = $a.attr({
+                    href: url,
+                    target: '_blank'
+                });
+
+                var storeData = getAppStoreData(store);
+
+                $img.attr({
+                    src: storeData.badge,
+                    alt: storeData.label
+                });
+
+                $a.append($img);
+                $li.append($a);
+                $detailContainer.find('.app-stores').append($li);
+            }
+        }
+
+        /**
+         *
+         * @param store
+         * @returns {*}
+         */
+        function getAppStoreData(store) {
+            var data;
+
+            switch (store) {
+                case 'apple':
+                    data = {
+                        label: 'App Store',
+                        badge: '/img/great-apps/badge_appstore.png'
+                    };
+                    break;
+
+                case 'google':
+                    data = {
+                        label: 'Google Play',
+                        badge: '/img/great-apps/badge_googleplay.png'
+                    };
+                    break;
+
+                default:
+                    data = {
+                        label: '',
+                        badge: ''
+                    };
+            }
+
+            return data;
+        }
+    };
 })();
-
-
-
-
-
-
-
