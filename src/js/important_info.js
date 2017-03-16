ff12b723129e4ec2999f714049069932a5ce57d6
mdeguchi <deguchi@asial.co.jp>
2017-03-16 18:35:45 +0900

    Modified

--- src/js/important_info.js
+++ src/js/important_info.js
@@ -12,9 +12,11 @@ $(function() {
 
   // 0: invisible, 1: visible
   var visibleFlag = 1;
+  var jaFlag = 1;
+  var enFlag = 0;
 
   // ja
-  var importantInfoJaEl = '<div class="cmn-important-info"><a href="/headline/fault.html" target="_blamk"><h2>';
+  var importantInfoJaEl = '<div class="cmn-important-info"><a href="/important-info/info-new-plan.html" target="_blamk"><h2>';
   importantInfoJaEl += '<span class="cmn-label color-red">重要</span>';
   importantInfoJaEl += 'Monacaの一部のプランを対象に';
   importantInfoJaEl += '<br class="visible-xs"> ';
@@ -23,7 +25,7 @@ $(function() {
   importantInfoJaEl += '</h2></a></div>';
 
   // en
-  var importantInfoEnEl = '<div class="cmn-important-info"><a href="/headline/fault.html" target="_blamk"><h2>';
+  var importantInfoEnEl = '<div class="cmn-important-info"><a href="/important-info/info-new-plan.html" target="_blamk"><h2>';
   importantInfoEnEl += '<span class="cmn-label color-red">重要</span>';
   importantInfoEnEl += 'Monacaの一部のプランを対象に';
   importantInfoEnEl += '<br class="visible-xs"> ';
@@ -32,15 +34,29 @@ $(function() {
   importantInfoEnEl += '</h2></a></div>';
 
   if (visibleFlag == 1) {
-    // ja
-    $('#important-info-ja-el').append(importantInfoJaEl);
+    if (jaFlag == 1) {
+      // ja
+      $('#important-info-ja-el').append(importantInfoJaEl);
+      
+      // Change CSS
+      $('body.pricing article.main .container').css('padding-top', '50px');
+      $('html[lang=ja] body.ci .subfeatures').css('margin-bottom', '24px');
+    }
 
-    // en
-    $('#important-info-en-el').append(importantInfoEnEl);
+    if(enFlag == 1) {
+      // en
+      $('#important-info-en-el').append(importantInfoEnEl);
 
-    // Change CSS
-    $('body.pricing article.main .container').css('padding-top', '50px');
-    $('body.ci .subfeatures').css('margin-bottom', '24px');
+      // Change CSS
+      $('body.pricing article.main .container').css('padding-top', '50px');
+      $('html[lang=en] body.ci .subfeatures').css('margin-bottom', '24px');
+
+    } else {
+
+      $('body.pricing article.main .container').css('padding-top', '50px');
+      $('html body.ci .subfeatures').css('margin-bottom', '90px');
+
+    }
+
   }
-
 });
