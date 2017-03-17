b2a51c7860f3ffd5afdd007929e970737b9e97f3
Katsutoshi Makino <makki@asial.co.jp>
2017-03-17 13:56:30 +0900

    modified plan change info

--- src/js/important_info.js
+++ src/js/important_info.js
@@ -11,44 +11,4 @@ $(function() {
    */
 
   // 0: invisible, 1: visible
-  var jaFlag = 1;
-  var enFlag = 0;
-
-  // ja
-  var importantInfoJaEl = '<div class="cmn-important-info"><a href="/important-info/info-new-plan.html" target="_blamk"><h2>';
-  importantInfoJaEl += '<span class="cmn-label color-red">重要</span>';
-  importantInfoJaEl += 'Monacaの一部のプランを対象に';
-  importantInfoJaEl += '<br class="visible-xs"> ';
-  importantInfoJaEl += '<span class="cmn-ipt-udt-linktext">料金プランの変更</span>';
-  importantInfoJaEl += ' をさせて頂きます。';
-  importantInfoJaEl += '</h2></a></div>';
-
-  // en
-  var importantInfoEnEl = '<div class="cmn-important-info"><a href="/important-info/info-new-plan.html" target="_blamk"><h2>';
-  importantInfoEnEl += '<span class="cmn-label color-red">重要</span>';
-  importantInfoEnEl += 'Monacaの一部のプランを対象に';
-  importantInfoEnEl += '<br class="visible-xs"> ';
-  importantInfoEnEl += '<span class="cmn-ipt-udt-linktext">料金プランの変更</span>';
-  importantInfoEnEl += ' をさせて頂きます。';
-  importantInfoEnEl += '</h2></a></div>';
-
-  if (jaFlag == 1) {
-    // ja
-    $('#important-info-ja-el').append(importantInfoJaEl);
-
-    // Change CSS
-    $('html[lang=ja] body.pricing article.main .container').css('padding-top', '50px');
-    $('html[lang=ja] body.ci .subfeatures').css('margin-bottom', '24px');
-  }
-
-  if(enFlag == 1) {
-    // en
-    $('#important-info-en-el').append(importantInfoEnEl);
-
-    // Change CSS
-    $('html[lang=en] body.pricing article.main .container').css('padding-top', '50px');
-    $('html[lang=en] body.ci .subfeatures').css('margin-bottom', '24px');
-
-  }
-
 });
