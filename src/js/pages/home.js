a8d286443bb4e8729c5b04d27e96cf93be9e1afb
Takuya Watanabe <takuya@asial.co.jp>
2017-03-21 20:37:29 +0900

    Modify home.js

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,67 +1,56 @@
-(function() {
-
+(function () {
   window.monacaPages = window.monacaPages || [];
 
-  var limit = 2;
-
-  monacaPages["/index.html"] = function (loginData) {
-    //Calling a function to get the list of news from the server. The false value of the "all" paramater tells the server
-    //to get only the news that have permision to be showed on the top page
-    getHeadline( { lang : window.LANG , type : "news_and_release" , limit : limit , all: false} , 
-      function(data) {
-        appendHeadline(
-          $(".headline-entries") , data
-        );
-
-        $(".headline-entry-toggle").on("click", "img", function() {
-          $img = $(this);
-          $entries = $img.parent().parent().find(".headline-sub-entries");
-          if ($entries.css("display") == "none") {
-            $img.attr("src", "/img/headline/ico_tri_downward.png");
-          } else {
-            $img.attr("src", "/img/headline/ico_tri_leftward.png");
-          }
-          $entries.slideToggle();
-        });
+  monacaPages["/index.html"] = function () {
+    var limit = 3;
+    getHeadline({lang: window.LANG, type: "news_and_release", limit: limit, all: false},
+      function (data) {
+        appendHeadline($(".headline-entries"), data);
       }
     );
 
-    //Caling a function to get the list of events. The number of events is defined by the limit parameter
-    getEntry( { lang : window.LANG , type : "news_and_release" , limit : limit} ,
-      function(data) {
-        appendEntry( $(".events-entries") , data);
+    getEventList({lang: window.LANG, limit: limit},
+      function (data) {
+        appendEvent($(".events-entries"), data);
       }
     );
-
-
   };
 
-
-  function getHeadline(options,success,fail) {
+  /**
+   * Get News and release list
+   * @param options
+   * @param success
+   * @param fail
+   */
+  function getHeadline(options, success, fail) {
     var lang = options.lang || 'en';
     var news_type = options.type || 'news_and_release';
-    var limit = options.limit || 50;
-    var all =options.all || true;
+    var limit = options.limit || 3;
+    var all = options.all || true;
 
-    $.ajax( {
-      type : "GET",
-      url : monacaApi.getBaseUrl() + "/" + lang + "/api/news/list",
-      dataType : "JSON",
-      contentType : "text/plain",
-      data : { type : news_type , limit : limit, all : all },
+    $.ajax({
+      type: "GET",
+      url: monacaApi.getBaseUrl() + "/" + lang + "/api/news/list",
+      dataType: "JSON",
+      contentType: "text/plain",
+      data: {type: news_type, limit: limit, all: all},
       xhrFields: {
         withCredentials: true
       },
-      success : success ,
-      fail : fail
-    } );
+      success: success,
+      fail: fail
+    });
   }
 
-
-  function appendHeadline( element , data ) {  
+  /**
+   * Append element to HeadLine
+   * @param element
+   * @param data
+   */
+  function appendHeadline(element, data) {
     var result = data.result;
 
-    if(result.length == 0){
+    if (result.length == 0) {
       $("#news-list").hide();
       return;
     }
@@ -72,76 +61,92 @@
         '  <dl>' +
         '    <dt>' + entry.date + '</dt>' +
         '    <dd>' +
-        '      <span class="headline-entry-comment-news"><a href="/headline/index.html#entry_'+entry.id+'" target="_blank">' + entry.body + '</a></span>' +
+        '      <span class="headline-entry-comment-news">' +
+        '<a href="/headline/index.html#entry_' + entry.id + '" target="_blank">' + entry.body + '</a></span>' +
         '    </dd>' +
         '  </dl>' +
         '</div>'
       );
     }
-  } 
-
-
-  function getEntry(options,success,fail) {
-    var lang = options.lang || 'en';
-    var news_type = options.type || 'news_and_release';
-    var limit = options.limit || 50;
-
-  $.ajax( {
-        type : "GET",
-        url : monacaApi.getBaseUrl() + "/" + lang + "/api/event/list",
-        dataType : "JSON",
-        contentType : "text/plain",
-        data : { type : news_type , limit : limit },
-        xhrFields: {
-          withCredentials: true
-        },
-        success : success ,
-        fail : fail
-      } );
   }
 
-  function appendEntry( element , data ) {  
-    var result = data.result;
+  /**
+   * Get event list
+   * @param options
+   * @param success
+   * @param fail
+   */
+  function getEventList(options, success, fail) {
+    var lang = options.lang || 'en';
+    var limit = options.limit || 50;
 
-    if(result.length == 0){
+    $.ajax({
+      type: "GET",
+      url: monacaApi.getBaseUrl() + "/" + lang + "/api/event/list",
+      dataType: "JSON",
+      contentType: "text/plain",
+      data: {limit: limit},
+      xhrFields: {
+        withCredentials: true
+      },
+      success: success,
+      fail: fail
+    });
+  }
+
+  /**
+   * Append element to event-list
+   * @param element
+   * @param data
+   */
+  function appendEvent(element, data) {
+    var result = data.result;
+    if (result.length == 0) {
       $("#events-list").hide();
       return;
     }
 
-    var list= element.html();      
-
+    var list = element.html();
     for (var i = 0; i < result.length; i++) {
       var entry = result[i];
-      
+      var categoryTag = createCategoryTag(entry);
 
-      var categoryTag = '<span class="category-on">';
-      var today = new Date();
-      var d = new Date(entry.date);
+      var text = '<tr id="entry_' + entry.id + '" class="events-entry">' +
+        '<th><span>' + entry.date + '</span></th>' +
+        '<td class="status_column">' + categoryTag + '</td>' +
+        '<td class="events-entry-comment">' +
+        '<span class="event-entry-comment-body"><a href="' + entry.url + '" target="_blank">' + entry.title + '</a></span>' +
+        '</td>' +
+        '</tr>';
 
-      if(d.getTime()<today.getTime()){
-         categoryTag = '<span class="category-finished">';
-      }
-
-      if (entry.category == 0) {
-        categoryTag += 'セミナー</span>';
-      }else if (entry.category == 1) {
-        categoryTag += '展示会カンファレンス</span>';
-      } else if (entry.category == 2) {
-        categoryTag += 'ワークショップ</span>';
-      }
-
-      
-      var text = '<tr id="entry_' + entry.id + '" class="events-entry">'+
-            '<th><span>' + entry.date +  '</span></th>' +
-            '<td class="status_column">' + categoryTag +  '</td>' +
-            '<td class="events-entry-comment"><span class="event-entry-comment-body"><a href="'+entry.url+'" target="_blank">' + entry.title + '</a></span></td>' +
-            '</tr>';
-
-        list+=text;  
+      list += text;
     }
 
-     element.html(list); 
-  } 
+    element.html(list);
+  }
 
+  /**
+   * Create category tag
+   * @param entry
+   * @returns {string}
+   */
+  function createCategoryTag(entry) {
+    var categoryTag = '';
+    var today = new Date();
 
+    if ((new Date(entry.date)).getTime() < today.getTime()) {
+      categoryTag = '<span class="category-finished">';
+    } else {
+      categoryTag = '<span class="category-on">';
+    }
+
+    if (entry.category == 0) {
+      categoryTag += 'セミナー</span>';
+    } else if (entry.category == 1) {
+      categoryTag += '展示会カンファレンス</span>';
+    } else if (entry.category == 2) {
+      categoryTag += 'ワークショップ</span>';
+    }
+    return categoryTag;
+  }
 })();
