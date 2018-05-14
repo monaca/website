710b9949501a5544862052f7aebaaf3d856c34d6
yong <yong@asial.co.jp>
2018-05-14 15:58:03 +0900

    call symfony API and drafting error information

--- src/js/pages/phonegap.js
+++ src/js/pages/phonegap.js
@@ -1,28 +1,54 @@
 (function() {
   window.monacaPages = window.monacaPages || [];
 
-  monacaPages["/phonegap/phonegap_enterprise_notification.html"] = function() {
+  monacaPages['/phonegap/phonegap_enterprise_notification.html'] = function(loginData) {
     formUtil.resetError();
 
+    loginData.autoDisplay = false;
     var btnNotifyMe = '#notify_me';
     var inputFormEmail = '#form_email';
+    var inputFormError= '#phonegap-enterprise-form-error';
 
-    $(btnNotifyMe).click(function() {
-      formUtil.resetError();
-      formUtil.disableAllInput();
+    loginData.onReady(function() {
+      $(btnNotifyMe).click(function() {
+        $('html,body').css('cursor', 'progress'); //waiting
+        formUtil.resetError();
+        formUtil.disableAllInput();
 
-      var email = $(inputFormEmail).val();
-      email = $.trim(email);
-      if (validEmail(email)) {
-        //do something
-        alert(email);
-        $(inputFormEmail).val('');
-        displayFinishPage();
-      } else {
-        alert('invalid email');
-      }
+        var email = $(inputFormEmail).val();
+        email = $.trim(email);
+        if (validEmail(email)) {
+          var sendData = {
+            'email': email,
+          };
+          $.ajax({
+            type: 'POST',
+            url: monacaApi.getBaseUrl() + '/' + window.LANG + '/api/subscribephonegapenterprise',
+            xhrFields: {
+              withCredentials: false
+            },
+            dataType: 'json',
+            data: sendData,
+            success: function (msg) {
+              $('html,body').css('cursor', 'default');
+              if (msg.status === 'success') {
+                displayFinishPage();
+              } else {
+                displayError('Something wrong. Please try again.');
+              }
+            },
+            error: function (msg) {
+              $('html,body').css('cursor', 'default');
+              location.href = '/error500.html';
+            }
+          });
+        } else {
+          $('html,body').css('cursor', 'default');
+          displayError('Invalid Email Address');
+        }
 
-      formUtil.enableAllInput();
+        formUtil.enableAllInput();
+      });
     });
 
     $(inputFormEmail).keypress(function(e) {
@@ -32,6 +58,12 @@
       }
     });
 
+    function displayError (errorMessage) {
+      formUtil.enableAllInput();
+      $(inputFormError).text(errorMessage);
+      $(inputFormError).css('display', 'block');
+    }
+
     function validEmail (email) {
       if (!email) return false;
       var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -40,6 +72,7 @@
     }
 
     function displayFinishPage() {
+      $(inputFormEmail).val('');
       location.href='/phonegap/thankyou.html';
     }
 
