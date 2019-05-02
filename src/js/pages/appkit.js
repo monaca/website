75061f0c5376962e96d558b2bd73807f8cf549dd
takuya <takuya@asial.co.jp>
2019-05-02 11:33:36 +0900

    modify appkit folder name

--- src/js/pages/appkit.js
+++ src/js/pages/appkit.js
@@ -3,10 +3,7 @@
   window.monacaPages = window.monacaPages || [];
 
   monacaPages["/appkit/index.html"] = function() {
-    myID = document.getElementById("myID");
-
     var myScrollFunc = function () {
-        var y = window.scrollY;
         if (isElementInView($('#hero-button'))) {
           $('#inquiry-button').fadeOut();
         } else {
