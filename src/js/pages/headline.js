1032d63d603d94fb5865dfe8175da68a4f0169d4
Bodor Gergely <bgolyoo@gmail.com>
2020-03-24 13:10:35 +0100

    Modified Learn Monaca Headline

--- src/js/pages/headline.js
+++ src/js/pages/headline.js
@@ -1,24 +1,27 @@
-(function() {
+(function () {
 
   window.monacaPages = window.monacaPages || [];
 
   var limit = 50;
 
-  monacaPages["/headline/index.html"] = function (loginData) {
+  monacaPages['/headline/index.html'] = function (loginData) {
 
-    getHeadline( { lang : window.LANG , type : "news_and_release" , limit : limit } ,
-      function(data) {
+
+    // appendHeadline($('.headline-entries'), mock);
+
+    getHeadline({ lang: window.LANG, type: 'news_and_release', limit: limit },
+      function (data) {
         appendHeadline(
-          $(".headline-entries") , data
+          $('.headline-entries'), data
         );
 
-        $(".headline-entry-toggle").on("click", "img", function() {
+        $('.headline-entry-toggle').on('click', 'img', function () {
           $img = $(this);
-          $entries = $img.parent().parent().find(".headline-sub-entries");
-          if ($entries.css("display") == "none") {
-            $img.attr("src", "/img/headline/ico_tri_downward.png");
+          $entries = $img.parent().parent().find('.headline-sub-entries');
+          if ($entries.css('display') == 'none') {
+            $img.attr('src', '/img/headline/ico_tri_downward.png');
           } else {
-            $img.attr("src", "/img/headline/ico_tri_leftward.png");
+            $img.attr('src', '/img/headline/ico_tri_leftward.png');
           }
           $entries.slideToggle();
         });
@@ -26,21 +29,21 @@
     );
   };
 
-  monacaPages["/headline/fault.html"] = function (loginData) {
+  monacaPages['/headline/fault.html'] = function (loginData) {
     var category = window.getQueryString('category');
-    getIssues( { lang : window.LANG , type : "fault" , limit : limit, category : category} ,
-      function(data) {
+    getIssues({ lang: window.LANG, type: 'fault', limit: limit, category: category },
+      function (data) {
         appendIssues(
-          window.LANG , $(".headline-entries") , data
+          window.LANG, $('.headline-entries'), data
         );
 
-        $(".headline-entry-toggle").on("click", "img", function() {
+        $('.headline-entry-toggle').on('click', 'img', function () {
           $img = $(this);
-          $entries = $img.parent().parent().find(".headline-sub-entries");
-          if ($entries.css("display") == "none") {
-            $img.attr("src", "/img/headline/ico_tri_downward.png");
+          $entries = $img.parent().parent().find('.headline-sub-entries');
+          if ($entries.css('display') == 'none') {
+            $img.attr('src', '/img/headline/ico_tri_downward.png');
           } else {
-            $img.attr("src", "/img/headline/ico_tri_leftward.png");
+            $img.attr('src', '/img/headline/ico_tri_leftward.png');
           }
           $entries.slideToggle();
         });
@@ -53,68 +56,74 @@
     var news_type = options.type || 'news_and_release';
     var limit = options.limit || 50;
 
-    $.ajax( {
-      type : "GET",
-      url : monacaApi.getBaseUrl() + "/" + lang + "/api/news/list",
-      dataType : "JSON",
-      contentType : "text/plain",
-      data : { type : news_type , limit : limit },
+    $.ajax({
+      type: 'GET',
+      url: monacaApi.getBaseUrl() + '/' + lang + '/api/news/list',
+      dataType: 'JSON',
+      contentType: 'text/plain',
+      data: { type: news_type, limit: limit },
       xhrFields: {
         withCredentials: true
       },
-      success : success ,
-      fail : fail
-    } );
+      success: success,
+      fail: fail
+    });
   }
 
-  function appendHeadline( element , data ) {  
+  function appendHeadline(element, data) {
     var result = data.result;
+    element.append(
+      '<div class="headline-entry header">' +
+      '  <div class="header-item">種別</div>' +
+      '  <div class="header-item">内容</div>' +
+      '  <div class="header-item">更新日</div>' +
+      '</div>'
+    );
     for (var i = 0; i < result.length; i++) {
       var entry = result[i];
       element.append(
         '<div id="entry_' + entry.id + '" class="headline-entry">' +
-        '  <dl>' +
-        '    <dt>' + entry.date + '</dt>' +
-        '    <dd>' +
-        '      <span class="headline-entry-comment-news">' + entry.body + '</span>' +
-        '    </dd>' +
-        '  </dl>' +
+        '  <div class="headline-entry-type">' +
+        '    <div class="headline-entry-type-badge">' + entry.type + '</div>' +
+        '  </div>' +
+        '  <div class="headline-entry-content">' + entry.body + '</div>' +
+        '  <div class="headline-entry-date">' + entry.date + '</div>' +
         '</div>'
       );
     }
-  } 
+  }
 
-  function getIssues(options,success,fail) {
+  function getIssues(options, success, fail) {
     var lang = options.lang || 'en';
     var limit = options.limit || 50;
     var category = options.category || '';
-    $.ajax( {
-      type : "GET",
-      url : monacaApi.getBaseUrl() + "/" + lang + "/api/issue/list",
-      dataType : "JSON",
-      contentType : "text/plain",
-      data : { limit : limit, category: category},
+    $.ajax({
+      type: 'GET',
+      url: monacaApi.getBaseUrl() + '/' + lang + '/api/issue/list',
+      dataType: 'JSON',
+      contentType: 'text/plain',
+      data: { limit: limit, category: category },
       xhrFields: {
         withCredentials: true
       },
-      success : success ,
-      fail : fail
-    } );
+      success: success,
+      fail: fail
+    });
   }
 
-  function appendIssues( lang, element , data ) {
+  function appendIssues(lang, element, data) {
     var result = data.result;
     var timeZone = lang == 'ja' ? 'JST' : 'UTC';
     for (var i = 0; i < result.length; i++) {
       var entry = result[i];
-      var shouldOpen = entry.shouldOpen;    
-      var icon = shouldOpen ? 'ico_tri_downward.png' : 'ico_tri_leftward.png' ;
+      var shouldOpen = entry.shouldOpen;
+      var icon = shouldOpen ? 'ico_tri_downward.png' : 'ico_tri_leftward.png';
       var statusTag = '<span class="status-solved">Solved</span>';
-      if (entry.statusCss == "fixing") {
+      if (entry.statusCss == 'fixing') {
         statusTag = '<span class="status-fixing">Fixing</span>';
-      } else if (entry.statusCss == "reported") {
+      } else if (entry.statusCss == 'reported') {
         statusTag = '<span class="status-reported">Reported</span>';
-      } 
+      }
       var detailsClass = shouldOpen ? '' : 'display: none;';
       element.append(
         '<div id="entry_' + entry.id + '" class="headline-entry">' +
@@ -122,27 +131,27 @@
         '  <dl>' +
         '    <dt>' + entry.date + '</dt>' +
         '    <dd>' +
-               statusTag +
+        statusTag +
         '      <span class="headline-entry-comment">' + entry.title + '</span>' +
         '    </dd>' +
         '  </dl>' +
         '  <div class="headline-sub-entries" style="' + detailsClass + '">' +
-        (function() {
-          if (entry.details && entry.details.length>0) {
-            var result = "";
-            for (var j=0;j<entry.details.length;j++) {
+        (function () {
+          if (entry.details && entry.details.length > 0) {
+            var result = '';
+            for (var j = 0; j < entry.details.length; j++) {
               var detail = entry.details[j];
               result += '<dl>' +
-                        '  <dt>' + detail.dateTime + ' ' + timeZone + '</dt>' +
-                        '  <dd>' + detail.description + '</dd>' +
-                        '</dl>';
+                '  <dt>' + detail.dateTime + ' ' + timeZone + '</dt>' +
+                '  <dd>' + detail.description + '</dd>' +
+                '</dl>';
             }
             return result;
           }
-          return "";
-        })() + 
+          return '';
+        })() +
         '  </div>' +
-        '</div>' 
+        '</div>'
       );
     }
   }
