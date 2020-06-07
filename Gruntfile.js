fc12051c3f4c7fdddad49a88faa1f0c019c1fdd1
takuya <takuya@asial.co.jp>
2020-06-07 18:41:56 +0900

    chore: update grunt to support es6 uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -553,7 +553,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-assemble');
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-uglify-es');
     grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
