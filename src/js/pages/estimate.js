ac7b868c11f8e130c0dd4d4d6187ac65e331d3e7
takuya <takuya@asial.co.jp>
2020-06-07 18:41:17 +0900

    feat: add budget field and update block-free-mail list

--- src/js/pages/estimate.js
+++ src/js/pages/estimate.js
@@ -9,17 +9,28 @@
     $('#send_feedback').on('click', function (event) {
       // disable submit button
       $('#send_feedback').prop('disabled', true);
+      
+      let budgetEle = $('#budget option:selected');
+      if (!budgetEle.val()) {
+        $('#content #budget + p.form-error').show();
+        $('#send_feedback').prop('disabled', false);
+        return;
+      }
 
       // setup parameters to send
       var data = {};
       $('#content input[type=text],textarea').each(function (index, el) {
         data[$(el).attr('name')] = $(el).val();
       });
+      data.comment = "ご予算:" + $('#budget option:selected').text() + "\n" + data.comment;
       
       $('.form-error').hide(); // hide old errors
 
-      var regexp = /^(?=.*@(gmail|yahoo|icloud)).+$/; //stop free mail domain
-      if (regexp.test($('input[name="email"]').val())) {
+      
+
+      const _dmains = freeMailDomains.join('|');
+      const regexp = `^(?=.*@(${_dmains})).+$`; //stop free mail domain
+      if ((new RegExp(regexp)).test($('input[name="email"]').val())) {
         $('#content input[name="email"] ~ p.form-error')
         .html("独自ドメインメールをご入力ください。").show();
 
@@ -63,4 +74,49 @@
       });
     });
   }
+
+  const freeMailDomains = [
+    'gmail.com',
+    'yahoo',
+    'hotmail',
+    'aol.com',
+    'msn.com',
+    'live.com',
+    'mac.com',
+    'facebook.com',
+    'icloud',
+    'outlook',
+    'ezweb.ne.jp',
+    'ido.ne.jp',
+    'biz.ezweb.ne.jp',
+    'augps.ezweb.ne.jp',
+    'uqmobile.jp',
+    'docomo.ne.jp',
+    'mopera.net',
+    'dwmail.jp',
+    'pdx.ne.jp',
+    'wcm.ne.jp',
+    'willcom.com',
+    'y-mobile.ne.jp',
+    'emnet.ne.jp',
+    'emobile-s.ne.jp',
+    'emobile.ne.jp',
+    'ymobile1.ne.jp',
+    'ymobile.ne.jp',
+    'jp-c.ne.jp',
+    'jp-d.ne.jp',
+    'jp-h.ne.jp',
+    'jp-k.ne.jp',
+    'jp-n.ne.jp',
+    'jp-q.ne.jp',
+    'jp-r.ne.jp',
+    'jp-s.ne.jp',
+    'jp-t.ne.jp',
+    'sky.tkc.ne.jp',
+    'sky.tkk.ne.jp',
+    'sky.tu-ka.ne.jp',
+    'disney.ne.jp',
+    'i.softbank.jp',
+    'softbank.ne.jp',
+  ]
 })();
